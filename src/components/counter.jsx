3fbcbd30881aff6083438e03d082942f65bb1aa6
Harry McAlister <harry.mcalist@gmail.com>
2022-12-01 18:48:58 +0000

    Implement a delete button for each Counter which uses props event handling

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,6 +18,11 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm">
           Increment
         </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2">
+          Delete
+        </button>
       </div>
     );
   }

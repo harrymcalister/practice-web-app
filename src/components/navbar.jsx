70f33f75faa81ab7e3a41cd8c31db8c1b143bdd4
Harry McAlister <harry.mcalist@gmail.com>
2022-12-03 11:15:35 +0000

    Add object destructuring to Counters and Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Navbar{" "}
-            <span className="badge badge-pill badge-secondary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
